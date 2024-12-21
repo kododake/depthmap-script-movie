7ce0dcbea05b40db34a91675eaf523f2daca609f
kododake <akur8664@gmail.com>
2024-12-21 19:44:20 +0900

    test

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 # Installs dependencies
 # Make sure to add to requirements.txt - it can be used for the standalone mode
 
-import launch
+import subprocess
 import platform
 import sys
 import importlib.metadata
@@ -9,22 +9,22 @@ import importlib.metadata
 # TODO: some dependencies apparently being reinstalled on every run. Investigate and fix.
 
 if sys.version_info < (3, 8):
-    launch.run_pip("install importlib-metadata", "importlib-metadata for depthmap script")
+    subprocess.run_pip("install importlib-metadata", "importlib-metadata for depthmap script")
     import importlib_metadata
 else:
     import importlib.metadata as importlib_metadata
-if not launch.is_installed('packaging'):
-    launch.run_pip("install packaging", "packaging requirement for depthmap script")
+if not subprocess.is_installed('packaging'):
+    subprocess.run_pip("install packaging", "packaging requirement for depthmap script")
 from packaging.version import Version
 
 def ensure(module_name, min_version=None):
-    if launch.is_installed(module_name):
+    if subprocess.is_installed(module_name):
         if min_version is None or Version(importlib_metadata.version(module_name)) >= Version(min_version):
             return
     requirement = f'{module_name}>={min_version}' if min_version is not None else module_name
     cmd = f'install "{requirement}"'
     msg = f'{requirement} requirement for depthmap script'
-    launch.run_pip(cmd, msg)
+    subprocess.run_pip(cmd, msg)
 
 
 ensure('timm', '0.9.2')  # For midas, specified just in case
@@ -38,8 +38,8 @@ ensure('vispy', '0.13.0')
 
 ensure('rembg', '2.0.50')
 
-if not launch.is_installed("moviepy"):
-    launch.run_pip('install "moviepy==1.0.2"', "moviepy requirement for depthmap script")
+if not subprocess.is_installed("moviepy"):
+    subprocess.run_pip('install "moviepy==1.0.2"', "moviepy requirement for depthmap script")
 ensure('transforms3d', '0.4.1')
 
 ensure('diffusers', '0.20.1')  # For Merigold
@@ -51,8 +51,8 @@ except:
     ensure('imageio-ffmpeg')
 
 
-if not launch.is_installed("networkx"):
-    launch.run_pip('install install "networkx==2.5"', "networkx requirement for depthmap script")
+if not subprocess.is_installed("networkx"):
+    subprocess.run_pip('install install "networkx==2.5"', "networkx requirement for depthmap script")
 if platform.system() == 'Windows':
     ensure('pyqt5')
 
@@ -70,7 +70,7 @@ def try_install_from_wheel(pkg_name: str, wheel_url: str):
     if get_installed_version(pkg_name) is not None:
         return
     try:
-        launch.run_pip(f"install {wheel_url}", f" {pkg_name} requirement for depthmap script")
+        subprocess.run_pip(f"install {wheel_url}", f" {pkg_name} requirement for depthmap script")
     except Exception as e:
         print('Failed to install wheel for Depth Anything support. It won\'t work.')
 try_install_from_wheel(
