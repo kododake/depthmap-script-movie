08fc31db1a6ec5695086a523160c2258768fd634
kododake <80098041+kododake@users.noreply.github.com>
2024-12-21 19:32:29 +0900

    Update next_vit.py

--- dmidas/backbones/next_vit.py
+++ dmidas/backbones/next_vit.py
@@ -1,9 +1,15 @@
 import timm
-
 import torch.nn as nn
-
 from pathlib import Path
 from .utils import activations, forward_default, get_activation
+from functools import partial
+import torch
+import torch.utils.checkpoint as checkpoint
+from einops import rearrange
+from timm.models.layers import DropPath, trunc_normal_
+from timm.models.registry import register_model
+
+NORM_EPS = 1e-5
 
 # thygate: just dropped the file in place here together with the single function import merge_pre_bn from Next_ViT repo which is no longer required :
 
@@ -14,20 +20,11 @@ from .utils import activations, forward_default, get_activation
 #start of file : Next_ViT/classification/nextvit.py :
 
 # Copyright (c) ByteDance Inc. All rights reserved.
-from functools import partial
-
-import torch
-import torch.utils.checkpoint as checkpoint
-from einops import rearrange
-from timm.models.layers import DropPath, trunc_normal_
-from timm.models.registry import register_model
-from torch import nn
 
 # function from Next_ViT/classification/utils.py : merge_pre_bn
 # copied here to get rid of Next_ViT repo dependancy
 def merge_pre_bn(module, pre_bn_1, pre_bn_2=None):
-    """ Merge pre BN to reduce inference runtime.
-    """
+    """ Merge pre BN to reduce inference runtime. """
     weight = module.weight.data
     if module.bias is None:
         zeros = torch.zeros(module.out_channels, device=weight.device).type(weight.type())
@@ -51,7 +48,7 @@ def merge_pre_bn(module, pre_bn_1, pre_bn_2=None):
         scale_invstd_2 = pre_bn_2.running_var.add(pre_bn_2.eps).pow(-0.5)
 
         extra_weight = scale_invstd_1 * pre_bn_1.weight * scale_invstd_2 * pre_bn_2.weight
-        extra_bias = scale_invstd_2 * pre_bn_2.weight *(pre_bn_1.bias - pre_bn_1.weight * pre_bn_1.running_mean * scale_invstd_1 - pre_bn_2.running_mean) + pre_bn_2.bias
+        extra_bias = scale_invstd_2 * pre_bn_2.weight * (pre_bn_1.bias - pre_bn_1.weight * pre_bn_1.running_mean * scale_invstd_1 - pre_bn_2.running_mean) + pre_bn_2.bias
 
     if isinstance(module, nn.Linear):
         extra_bias = weight @ extra_bias
@@ -67,19 +64,8 @@ def merge_pre_bn(module, pre_bn_1, pre_bn_2=None):
     module.weight.data = weight
     module.bias.data = bias
 
-
-
-NORM_EPS = 1e-5
-
-
 class ConvBNReLU(nn.Module):
-    def __init__(
-            self,
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            groups=1):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, groups=1):
         super(ConvBNReLU, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
                               padding=1, groups=groups, bias=False)
@@ -92,22 +78,16 @@ class ConvBNReLU(nn.Module):
         x = self.act(x)
         return x
 
-
 def _make_divisible(v, divisor, min_value=None):
     if min_value is None:
         min_value = divisor
     new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
     if new_v < 0.9 * v:
         new_v += divisor
     return new_v
 
-
 class PatchEmbed(nn.Module):
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 stride=1):
+    def __init__(self, in_channels, out_channels, stride=1):
         super(PatchEmbed, self).__init__()
         norm_layer = partial(nn.BatchNorm2d, eps=NORM_EPS)
         if stride == 2:
@@ -126,11 +106,7 @@ class PatchEmbed(nn.Module):
     def forward(self, x):
         return self.norm(self.conv(self.avgpool(x)))
 
-
 class MHCA(nn.Module):
-    """
-    Multi-Head Convolutional Attention
-    """
     def __init__(self, out_channels, head_dim):
         super(MHCA, self).__init__()
         norm_layer = partial(nn.BatchNorm2d, eps=NORM_EPS)
@@ -147,7 +123,6 @@ class MHCA(nn.Module):
         out = self.projection(out)
         return out
 
-
 class Mlp(nn.Module):
     def __init__(self, in_features, out_features=None, mlp_ratio=None, drop=0., bias=True):
         super().__init__()
@@ -158,9 +133,6 @@ class Mlp(nn.Module):
         self.conv2 = nn.Conv2d(hidden_dim, out_features, kernel_size=1, bias=bias)
         self.drop = nn.Dropout(drop)
 
-    def merge_bn(self, pre_norm):
-        merge_pre_bn(self.conv1, pre_norm)
-
     def forward(self, x):
         x = self.conv1(x)
         x = self.act(x)
@@ -169,13 +141,8 @@ class Mlp(nn.Module):
         x = self.drop(x)
         return x
 
-
 class NCB(nn.Module):
-    """
-    Next Convolution Block
-    """
-    def __init__(self, in_channels, out_channels, stride=1, path_dropout=0,
-                 drop=0, head_dim=32, mlp_ratio=3):
+    def __init__(self, in_channels, out_channels, stride=1, path_dropout=0, drop=0, head_dim=32, mlp_ratio=3):
         super(NCB, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -191,28 +158,15 @@ class NCB(nn.Module):
         self.mlp_path_dropout = DropPath(path_dropout)
         self.is_bn_merged = False
 
-    def merge_bn(self):
-        if not self.is_bn_merged:
-            self.mlp.merge_bn(self.norm)
-            self.is_bn_merged = True
-
     def forward(self, x):
         x = self.patch_embed(x)
         x = x + self.attention_path_dropout(self.mhca(x))
-        if not torch.onnx.is_in_onnx_export() and not self.is_bn_merged:
-            out = self.norm(x)
-        else:
-            out = x
+        out = self.norm(x)
         x = x + self.mlp_path_dropout(self.mlp(out))
         return x
 
-
 class E_MHSA(nn.Module):
-    """
-    Efficient Multi-Head Self Attention
-    """
-    def __init__(self, dim, out_dim=None, head_dim=32, qkv_bias=True, qk_scale=None,
-                 attn_drop=0, proj_drop=0., sr_ratio=1):
+    def __init__(self, dim, out_dim=None, head_dim=32, qkv_bias=True, qk_scale=None, attn_drop=0, proj_drop=0., sr_ratio=1):
         super().__init__()
         self.dim = dim
         self.out_dim = out_dim if out_dim is not None else dim
@@ -232,16 +186,6 @@ class E_MHSA(nn.Module):
             self.norm = nn.BatchNorm1d(dim, eps=NORM_EPS)
         self.is_bn_merged = False
 
-    def merge_bn(self, pre_bn):
-        merge_pre_bn(self.q, pre_bn)
-        if self.sr_ratio > 1:
-            merge_pre_bn(self.k, pre_bn, self.norm)
-            merge_pre_bn(self.v, pre_bn, self.norm)
-        else:
-            merge_pre_bn(self.k, pre_bn)
-            merge_pre_bn(self.v, pre_bn)
-        self.is_bn_merged = True
-
     def forward(self, x):
         B, N, C = x.shape
         q = self.q(x)
@@ -250,8 +194,7 @@ class E_MHSA(nn.Module):
         if self.sr_ratio > 1:
             x_ = x.transpose(1, 2)
             x_ = self.sr(x_)
-            if not torch.onnx.is_in_onnx_export() and not self.is_bn_merged:
-                x_ = self.norm(x_)
+            x_ = self.norm(x_)
             x_ = x_.transpose(1, 2)
             k = self.k(x_)
             k = k.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(0, 2, 3, 1)
@@ -272,15 +215,8 @@ class E_MHSA(nn.Module):
         x = self.proj_drop(x)
         return x
 
-
 class NTB(nn.Module):
-    """
-    Next Transformer Block
-    """
-    def __init__(
-            self, in_channels, out_channels, path_dropout, stride=1, sr_ratio=1,
-            mlp_ratio=2, head_dim=32, mix_block_ratio=0.75, attn_drop=0, drop=0,
-    ):
+    def __init__(self, in_channels, out_channels, path_dropout, stride=1, sr_ratio=1, mlp_ratio=2, head_dim=32, mix_block_ratio=0.75, attn_drop=0, drop=0):
         super(NTB, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -292,8 +228,7 @@ class NTB(nn.Module):
 
         self.patch_embed = PatchEmbed(in_channels, self.mhsa_out_channels, stride)
         self.norm1 = norm_func(self.mhsa_out_channels)
-        self.e_mhsa = E_MHSA(self.mhsa_out_channels, head_dim=head_dim, sr_ratio=sr_ratio,
-                             attn_drop=attn_drop, proj_drop=drop)
+        self.e_mhsa = E_MHSA(self.mhsa_out_channels, head_dim=head_dim, sr_ratio=sr_ratio, attn_drop=attn_drop, proj_drop=drop)
         self.mhsa_path_dropout = DropPath(path_dropout * mix_block_ratio)
 
         self.projection = PatchEmbed(self.mhsa_out_channels, self.mhca_out_channels, stride=1)
@@ -306,20 +241,11 @@ class NTB(nn.Module):
 
         self.is_bn_merged = False
 
-    def merge_bn(self):
-        if not self.is_bn_merged:
-            self.e_mhsa.merge_bn(self.norm1)
-            self.mlp.merge_bn(self.norm2)
-            self.is_bn_merged = True
-
     def forward(self, x):
         x = self.patch_embed(x)
         B, C, H, W = x.shape
-        if not torch.onnx.is_in_onnx_export() and not self.is_bn_merged:
-            out = self.norm1(x)
-        else:
-            out = x
-        out = rearrange(out, "b c h w -> b (h w) c")  # b n c
+        out = self.norm1(x)
+        out = rearrange(out, "b c h w -> b (h w) c")
         out = self.mhsa_path_dropout(self.e_mhsa(out))
         x = x + rearrange(out, "b (h w) c -> b c h w", h=H)
 
@@ -327,31 +253,18 @@ class NTB(nn.Module):
         out = out + self.mhca_path_dropout(self.mhca(out))
         x = torch.cat([x, out], dim=1)
 
-        if not torch.onnx.is_in_onnx_export() and not self.is_bn_merged:
-            out = self.norm2(x)
-        else:
-            out = x
+        out = self.norm2(x)
         x = x + self.mlp_path_dropout(self.mlp(out))
         return x
 
-
 class NextViT(nn.Module):
-    def __init__(self, stem_chs, depths, path_dropout, attn_drop=0, drop=0, num_classes=1000,
-                 strides=[1, 2, 2, 2], sr_ratios=[8, 4, 2, 1], head_dim=32, mix_block_ratio=0.75,
-                 use_checkpoint=False):
+    def __init__(self, stem_chs, depths, path_dropout, attn_drop=0, drop=0, num_classes=1000, strides=[1, 2, 2, 2], sr_ratios=[8, 4, 2, 1], head_dim=32, mix_block_ratio=0.75, use_checkpoint=False):
         super(NextViT, self).__init__()
         self.use_checkpoint = use_checkpoint
 
-        self.stage_out_channels = [[96] * (depths[0]),
-                                   [192] * (depths[1] - 1) + [256],
-                                   [384, 384, 384, 384, 512] * (depths[2] // 5),
-                                   [768] * (depths[3] - 1) + [1024]]
+        self.stage_out_channels = [[96] * (depths[0]), [192] * (depths[1] - 1) + [256], [384, 384, 384, 384, 512] * (depths[2] // 5), [768] * (depths[3] - 1) + [1024]]
 
-        # Next Hybrid Strategy
-        self.stage_block_types = [[NCB] * depths[0],
-                                  [NCB] * (depths[1] - 1) + [NTB],
-                                  [NCB, NCB, NCB, NCB, NTB] * (depths[2] // 5),
-                                  [NCB] * (depths[3] - 1) + [NTB]]
+        self.stage_block_types = [[NCB] * depths[0], [NCB] * (depths[1] - 1) + [NTB], [NCB, NCB, NCB, NCB, NTB] * (depths[2] // 5), [NCB] * (depths[3] - 1) + [NTB]]
 
         self.stem = nn.Sequential(
             ConvBNReLU(3, stem_chs[0], kernel_size=3, stride=2),
@@ -362,26 +275,20 @@ class NextViT(nn.Module):
         input_channel = stem_chs[-1]
         features = []
         idx = 0
-        dpr = [x.item() for x in torch.linspace(0, path_dropout, sum(depths))]  # stochastic depth decay rule
+        dpr = [x.item() for x in torch.linspace(0, path_dropout, sum(depths))]
         for stage_id in range(len(depths)):
             numrepeat = depths[stage_id]
             output_channels = self.stage_out_channels[stage_id]
             block_types = self.stage_block_types[stage_id]
             for block_id in range(numrepeat):
-                if strides[stage_id] == 2 and block_id == 0:
-                    stride = 2
-                else:
-                    stride = 1
+                stride = 2 if strides[stage_id] == 2 and block_id == 0 else 1
                 output_channel = output_channels[block_id]
                 block_type = block_types[block_id]
                 if block_type is NCB:
-                    layer = NCB(input_channel, output_channel, stride=stride, path_dropout=dpr[idx + block_id],
-                                drop=drop, head_dim=head_dim)
+                    layer = NCB(input_channel, output_channel, stride=stride, path_dropout=dpr[idx + block_id], drop=drop, head_dim=head_dim)
                     features.append(layer)
                 elif block_type is NTB:
-                    layer = NTB(input_channel, output_channel, path_dropout=dpr[idx + block_id], stride=stride,
-                                sr_ratio=sr_ratios[stage_id], head_dim=head_dim, mix_block_ratio=mix_block_ratio,
-                                attn_drop=attn_drop, drop=drop)
+                    layer = NTB(input_channel, output_channel, path_dropout=dpr[idx + block_id], stride=stride, sr_ratio=sr_ratios[stage_id], head_dim=head_dim, mix_block_ratio=mix_block_ratio, attn_drop=attn_drop, drop=drop)
                     features.append(layer)
                 input_channel = output_channel
             idx += numrepeat
@@ -398,86 +305,32 @@ class NextViT(nn.Module):
         print('initialize_weights...')
         self._initialize_weights()
 
-    def merge_bn(self):
-        self.eval()
-        for idx, module in self.named_modules():
-            if isinstance(module, NCB) or isinstance(module, NTB):
-                module.merge_bn()
-
-    def _initialize_weights(self):
-        for n, m in self.named_modules():
-            if isinstance(m, (nn.BatchNorm2d, nn.GroupNorm, nn.LayerNorm, nn.BatchNorm1d)):
-                nn.init.constant_(m.weight, 1.0)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                trunc_normal_(m.weight, std=.02)
-                if hasattr(m, 'bias') and m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Conv2d):
-                trunc_normal_(m.weight, std=.02)
-                if hasattr(m, 'bias') and m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
     def forward(self, x):
         x = self.stem(x)
         for idx, layer in enumerate(self.features):
-            if self.use_checkpoint:
-                x = checkpoint.checkpoint(layer, x)
-            else:
-                x = layer(x)
+            x = checkpoint.checkpoint(layer, x) if self.use_checkpoint else layer(x)
         x = self.norm(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.proj_head(x)
         return x
 
-
-@register_model
+# Define the models
 def nextvit_small(pretrained=False, pretrained_cfg=None, **kwargs):
     model = NextViT(stem_chs=[64, 32, 64], depths=[3, 4, 10, 3], path_dropout=0.1, **kwargs)
     return model
 
-
-@register_model
 def nextvit_base(pretrained=False, pretrained_cfg=None, **kwargs):
     model = NextViT(stem_chs=[64, 32, 64], depths=[3, 4, 20, 3], path_dropout=0.2, **kwargs)
     return model
 
-
-@register_model
 def nextvit_large(pretrained=False, pretrained_cfg=None, **kwargs):
     model = NextViT(stem_chs=[64, 32, 64], depths=[3, 4, 30, 3], path_dropout=0.2, **kwargs)
     return model
 
-# end of Next_ViT/classification/nextvit.py
+# Conditional registration
+if not timm.models.registry.is_model('nextvit_small'):
+    register_model(nextvit_small)
 
-
-def forward_next_vit(pretrained, x):
-    return forward_default(pretrained, x, "forward")
-
-
-def _make_next_vit_backbone(
-        model,
-        hooks=[2, 6, 36, 39],
-):
-    pretrained = nn.Module()
-
-    pretrained.model = model
-    pretrained.model.features[hooks[0]].register_forward_hook(get_activation("1"))
-    pretrained.model.features[hooks[1]].register_forward_hook(get_activation("2"))
-    pretrained.model.features[hooks[2]].register_forward_hook(get_activation("3"))
-    pretrained.model.features[hooks[3]].register_forward_hook(get_activation("4"))
-
-    pretrained.activations = activations
-
-    return pretrained
-
-
-def _make_pretrained_next_vit_large_6m(hooks=None):
-    model = timm.create_model("nextvit_large")
-
-    hooks = [2, 6, 36, 39] if hooks == None else hooks
-    return _make_next_vit_backbone(
-        model,
-        hooks=hooks,
-    )
+if not timm.models.registry.is_model('nextvit_base'):
+    register
