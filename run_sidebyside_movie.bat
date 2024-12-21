c83fccd6febde372651c91c8ba84b41589b6c07d
kododake <80098041+kododake@users.noreply.github.com>
2024-12-21 20:29:29 +0900

    Update run_sidebyside_movie.bat

--- run_sidebyside_movie.bat
+++ run_sidebyside_movie.bat
@@ -1,3 +1,4 @@
+
 @echo off
 REM Get the directory of the currently executed script
 set SCRIPT_DIR=%~dp0
@@ -7,4 +8,10 @@ cd /d "%SCRIPT_DIR%"
 
 REM Activate the virtual environment
 CALL venv\Scripts\activate
+
+REM Ensure GPU usage by setting the environment variable for CUDA
+set CUDA_VISIBLE_DEVICES=0
+
+REM Run the conversion script
+python run_sidebyside_movie.py
 pause
